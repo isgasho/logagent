4e1e0c5ea08294a06f9a7d27b48dbdd95d28111a
Hank <853160447@qq.com>
2019-10-28 11:13:02 +0800

    main

--- main.go
+++ main.go
@@ -34,8 +34,8 @@ func WriteLog2Ws() {
 	}
 }
 
-// ErrMonitor is a structure used for serializing/deserializing data in Elasticsearch.
-type ErrMonitor struct {
+// Tweet is a structure used for serializing/deserializing data in Elasticsearch.
+type Tweet struct {
 	User     string                `json:"user"`
 	Message  string                `json:"message"`
 	Retweets int                   `json:"retweets"`
@@ -46,63 +46,65 @@ type ErrMonitor struct {
 	Suggest  *elastic.SuggestField `json:"suggest_field,omitempty"`
 }
 
-const mapping = `
-{
-	"settings":{
-		"number_of_shards": 1,
-		"number_of_replicas": 0
-	},
-	"mappings":{
-		"tweet":{
-			"properties":{
-				"user":{
-					"type":"keyword"
-				},
-				"message":{
-					"type":"text",
-					"store": true,
-					"fielddata": true
-				},
-				"image":{
-					"type":"keyword"
-				},
-				"created":{
-					"type":"date"
-				},
-				"tags":{
-					"type":"keyword"
-				},
-				"location":{
-					"type":"geo_point"
-				},
-				"suggest_field":{
-					"type":"completion"
-				}
-			}
-		}
-	}
-}`
+//const mapping = `
+//{
+//	"settings":{
+//		"number_of_shards": 1,
+//		"number_of_replicas": 0
+//	},
+//	"mappings":{
+//		"tweet":{
+//			"properties":{
+//				"user":{
+//					"type":"keyword"
+//				},
+//				"message":{
+//					"type":"text",
+//					"store": true,
+//					"fielddata": true
+//				},
+//				"image":{
+//					"type":"keyword"
+//				},
+//				"created":{
+//					"type":"date"
+//				},
+//				"tags":{
+//					"type":"keyword"
+//				},
+//				"location":{
+//					"type":"geo_point"
+//				},
+//				"suggest_field":{
+//					"type":"completion"
+//				}
+//			}
+//		}
+//	}
+//}`
 
 
 func main(){
 	//StartGetLogServer("access.log")
 
+	// Starting with elastic.v5, you must pass a context to execute each service
 	ctx := context.Background()
 
-	config := &config.Config{URL:"http://localhost:9200"}
+	config := &config.Config{URL:"http://127.0.0.1:9200"}
 	client, err := elastic.NewClientFromConfig(config)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	exists, err := client.IndexExists("weberr-2019.10.27").Do(ctx)
+	exists, err := client.IndexExists("weberr-2019.10.28").Do(ctx)
 	if err != nil{
 		fmt.Println(err)
+		return
 	}
 
 	if !exists{
-		createIndex, err := client.CreateIndex("weberr-2019.10.27").BodyString(mapping).Do(ctx)
+		createIndex, err := client.CreateIndex("weberr-2019.10.28").Do(ctx)
 		if 	err != nil{
 			fmt.Println(err)
 		}
@@ -111,22 +113,24 @@ func main(){
 		}
 	}
 
-	errMonitor1 := ErrMonitor{User: "olivere", Message: "Take Five", Retweets: 0}
+	tweet1  := Tweet{User: "olivere", Message: "Take Five", Retweets: 0}
 	put1, err := client.Index().
-		Index("weberr-2019.10.27").
+		Index("weberr-2019.10.28").
 		Type("tweet").
 		Id("1").
-		BodyJson(errMonitor1).
+		BodyJson(tweet1).
 		Do(ctx)
+
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
+
 	fmt.Printf("Indexed tweet %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
 
 	//Get tweet with specified ID
 	get1, err := client.Get().
-		Index("twitter").
+		Index("weberr-2019.10.28").
 		Type("tweet").
 		Id("1").
 		Do(ctx)
@@ -138,4 +142,7 @@ func main(){
 	if get1.Found {
 		fmt.Printf("Got document %s in version %d from index %s, type %s\n", get1.Id, get1.Version, get1.Index, get1.Type)
 	}
+
+	fmt.Println("执行到这里结束")
+	return
 }
